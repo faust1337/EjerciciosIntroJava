8f2176d00e2524c58fdc92242844c8f391fda8ca
Fausto Raimundo <fausto.raimundo@hotmail.com>
2022-09-15 22:13:41 -0300

    Cambié el ejercicio 11 para que haya un bucle en el cual, si no ingreso S/N, no me permita salir de la opción 5. Más comentarios en el código.

--- Ejercicio11/src/ejercicio11/Ejercicio11.java
+++ Ejercicio11/src/ejercicio11/Ejercicio11.java
@@ -31,14 +31,18 @@ public class Ejercicio11 {
                     break;
                 case 5:
                     System.out.print("¿Desea salir? S/N ");
-                    opc2 = leer.next().charAt(0);
-                    if (opc2 == 'S' || opc2 == 's'){
-                        break;
-                    }else {
-                        opc = 0;
-                        break;
+                    opc2 = leer.next().charAt(0); //Leo la opción, que la deposita en char opc2                  
+                    while(opc2!='s' && opc2!='S' && opc2!='n' && opc2!='N'){ //Si la opción que ingresó, es distinta
+                        System.out.println("Opción inválida. Reingrese.");   //de s, S, n o N, entonces me pide de nuevo
+                        System.out.print("¿Desea salir? S/N ");              //hasta que ingrese una de esas 4.
+                        opc2 = leer.next().charAt(0);
                     }
-                    
+                    if (opc2 == 'S' || opc2 == 's'){//Si desea salir, termina el switch y sale del do-while.
+                        break;
+                    }else { //Si no desea salir (hasta acá, salió del while anterior únicamente con S o N. En este caso, N.)
+                        opc = 0; //fuerza a que la opción elegida al principio, sea 0 en vez de 5. Por lo que...
+                        break;//... con el break, termina el switch, pero vuelve a mostrar el menú.
+                    }                    
                 default:
                     System.out.println("Opción inválida.");
                     break;
