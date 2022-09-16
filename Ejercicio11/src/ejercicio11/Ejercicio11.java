439c74d488bed81f04d3aeb57213b7f55edd9dd8
Fausto Raimundo <fausto.raimundo@hotmail.com>
2022-09-15 21:50:47 -0300

    listo el 11.

--- Ejercicio11/src/ejercicio11/Ejercicio11.java
+++ Ejercicio11/src/ejercicio11/Ejercicio11.java
@@ -8,9 +8,8 @@ public class Ejercicio11 {
         Scanner leer = new Scanner(System.in);
         System.out.print("Ingrese un número para operar: "); int num1 = leer.nextInt();
         System.out.print("Ingrese otro número para operar: "); int num2 = leer.nextInt();
-        int opc;
-        /*menu*/
-        do{
+        int opc; char opc2;
+        do{   
             System.out.println("1. Sumar");
             System.out.println("2. Restar");
             System.out.println("3. Multiplicar");
@@ -30,16 +29,21 @@ public class Ejercicio11 {
                 case 4:
                     System.out.println("Resultado: " + num1/num2);
                     break;
+                case 5:
+                    System.out.print("¿Desea salir? S/N ");
+                    opc2 = leer.next().charAt(0);
+                    if (opc2 == 'S' || opc2 == 's'){
+                        break;
+                    }else {
+                        opc = 0;
+                        break;
+                    }
+                    
+                default:
+                    System.out.println("Opción inválida.");
+                    break;
             }
-            if (opc == 5){
-                System.out.println("¿Está seguro que desea salir? S/N ");
-                String salir = leer.nextLine();
-                if (salir.equals("S")){
-                    System.out.println("Saliste.");
-                }else{
-                    opc = 0;
-                }
-            }
-        }while(opc!=5);
+        }while(opc != 5);
+        System.out.println("Gracias por usar nuestro programa.");
     }    
 }
